c0e242ce09a054f681188b5a33fa4a069fa71247
Mouhari Mouhamed <mmouhamed@healthemed.com>
2024-10-25 08:17:26 -0500

    remove different page number due to compiling error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,13 +7,13 @@ export default function Home() {
       <h1>All Quizzes</h1>
       <ul>
         <li>
-          <a href="/quiz/1">Quiz 1</a>
+          <a href="/quiz">Quiz 1</a>
         </li>
         <li>
-          <a href="/quiz/2">Quiz 2</a>
+          <a href="/quiz">Quiz 2</a>
         </li>
         <li>
-          <a href="/quiz/3">Quiz 3</a>
+          <a href="/quiz">Quiz 3</a>
         </li>
       </ul>
     </section>
