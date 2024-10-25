8c4635e94117766e153a6912ca191c1c743fa418
Mouhari Mouhamed <mmouhamed@healthemed.com>
2024-10-25 08:20:23 -0500

    remove param in quiz/page.tsx

--- src/app/quiz/page.tsx
+++ src/app/quiz/page.tsx
@@ -1,7 +1,7 @@
-export default function QuizPage({params}: {params: {id: string} }) {
+export default function QuizPage() {
     return(
         <section>
-            <h1>Quiz {params.id}</h1>
+            <h1>Quiz</h1>
         </section>
     )
 }
